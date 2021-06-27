b437aaf7bbe6b2b7961d2860abc3857199c21b19
iHatePhysics <Sanskar218.chaware@gmail.com>
2021-06-27 12:45:29 +0530

    Memory Game Update!

--- Day 10 - Memory Game/index.js
+++ Day 10 - Memory Game/index.js
@@ -1,97 +1,117 @@
-const memoryGame = document.querySelector('.memory-game');
-var nos =['computer','energy','fire','game-console','gamepad','headphones','laptop','palm-tree','sunny','sword'];
-var deck = [...nos,...nos];
+const memoryGame = document.querySelector(".memory-game");
+var nos = [
+  "computer",
+  "energy",
+  "fire",
+  "game-console",
+  "gamepad",
+  "headphones",
+  "laptop",
+  "palm-tree",
+  "sunny",
+  "sword",
+];
+var deck = [...nos, ...nos];
 
 function shuffle(array) {
-    array.sort(() => Math.random() - 0.5);
+  array.sort(() => Math.random() - 0.5);
 }
 
 shuffle(deck);
-console.log(deck[1]);
-for (i=0;i<20;i++){
-    let memoryCard = document.createElement('div');
-    memoryCard.setAttribute("class","memory-card");
-    memoryCard.setAttribute("data-id",`${deck[i]}`);
-    let frontFace = document.createElement('div');
-    frontFace.setAttribute("class","front-face");
-    frontFace.style.backgroundImage = `url('images/${deck[i]}.png')`;
-    let backFace = document.createElement('div');
-    backFace.setAttribute("class","back-face");
-    memoryCard.appendChild(frontFace);
-    memoryCard.appendChild(backFace);
-    memoryGame.appendChild(memoryCard);
+for (i = 0; i < 20; i++) {
+  let memoryCard = document.createElement("div");
+  memoryCard.setAttribute("class", "memory-card");
+  memoryCard.setAttribute("data-id", `${deck[i]}`);
+  let frontFace = document.createElement("div");
+  frontFace.setAttribute("class", "front-face");
+  frontFace.style.backgroundImage = `url('images/${deck[i]}.png')`;
+  let backFace = document.createElement("div");
+  backFace.setAttribute("class", "back-face");
+  memoryCard.appendChild(frontFace);
+  memoryCard.appendChild(backFace);
+  memoryGame.appendChild(memoryCard);
 }
 
-const cards = document.querySelectorAll('.memory-card');
+const cards = document.querySelectorAll(".memory-card");
 
 let hasFlippedCard = false;
 let firstCard, secondCard;
 let lockBoard = false;
 var matches = 0;
-var moves =0;
+var moves = 0;
 
-function flipcard(){
-    if(lockBoard) return;
-    if(this === firstCard) return;
+function flipcard() {
+  if (lockBoard) return;
+  if (this === firstCard) return;
 
-    this.classList.add('flip');
+  this.classList.add("flip");
 
-    if (!hasFlippedCard){
-        hasFlippedCard = true;
-        firstCard = this;
-    }else{
-        hasFlippedCard = false;
-        secondCard = this
-        //console.log(firstCard.dataset.id);
-        //console.log(secondCard.dataset.id);
-        checkForMatch();
+  if (!hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = this;
+  } else {
+    hasFlippedCard = false;
+    secondCard = this;
+    //console.log(firstCard.dataset.id);
+    //console.log(secondCard.dataset.id);
+    checkForMatch();
+  }
+}
+
+function checkForMatch() {
+  if (firstCard.dataset.id === secondCard.dataset.id) {
+    disableCards();
+    matches += 1;
+    moves += 1;
+    document.querySelector(".matches").innerHTML = matches;
+    document.querySelector(".moves").innerHTML = moves;
+    if (matches === 10) {
+      finishGame();
     }
-};
-
-function checkForMatch(){
-    if(firstCard.dataset.id === secondCard.dataset.id){
-        disableCards();
-        matches += 1;
-        moves += 1;
-        document.querySelector('.matches').innerHTML = matches;
-        document.querySelector('.moves').innerHTML = moves;
-        if(matches === 10){
-            finishGame();
-        }
-    }
-    else{
-        unFlip();
-        moves +=1
-        document.querySelector('.moves').innerHTML = moves;
-    }
+  } else {
+    unFlip();
+    moves += 1;
+    document.querySelector(".moves").innerHTML = moves;
+  }
 }
 
-function disableCards(){
-    firstCard.removeEventListener('click',flipcard);
-    secondCard.removeEventListener('click',flipcard);
+function disableCards() {
+  firstCard.removeEventListener("click", flipcard);
+  secondCard.removeEventListener("click", flipcard);
 }
 
-function unFlip(){
-    lockBoard = true;
+function unFlip() {
+  lockBoard = true;
 
-    setTimeout(()=>{
-        firstCard.classList.remove('flip');
-        secondCard.classList.remove('flip');
-        //lockBoard = false;
-        resetBoard();
-    },750);
+  setTimeout(() => {
+    firstCard.classList.remove("flip");
+    secondCard.classList.remove("flip");
+    //lockBoard = false;
+    resetBoard();
+  }, 750);
 }
 
-function resetBoard(){
-    [hasFlippedCard, lockBoard] = [false,false];
-    [firstCard,secondCard]=[null,null];
+function resetBoard() {
+  [hasFlippedCard, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
 }
 
-cards.forEach(card => card.addEventListener('click', flipcard));
+cards.forEach((card) => card.addEventListener("click", flipcard));
 
-function finishGame(){
-    document.querySelector('.game-won').style.display = "flex";
+function finishGame() {
+  document.querySelector(".game-won").style.display = "flex";
 }
+
+function showCards() {
+  cards.forEach((card) => card.classList.add("flip"));
+  setTimeout(() => {
+    cards.forEach((card) => card.classList.remove("flip"));
+  }, 2000);
+}
+setTimeout(() => {
+  showCards();
+}, 1000);
+
 // if(matches === 10){
 //     alert("game won!")
 // }
