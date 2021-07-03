807bdacfd9144e827d330bf510d8387a8516b708
iHatePhysics <Sanskar218.chaware@gmail.com>
2021-07-03 18:20:54 +0530

    Day 12 Done!

---
+++ day-12 To Do List/index.js
@@ -0,0 +1,154 @@
+// CODE EXPLAINED channel
+
+// Select the Elements
+const clear = document.querySelector(".clear");
+const dateElement = document.getElementById("date");
+const list = document.getElementById("list");
+const input = document.getElementById("task-input");
+const addbtn = document.querySelector(".add-task");
+// Classes names
+const CHECK = "checked";
+const UNCHECK = "uncheck";
+const LINE_THROUGH = "lineThrough";
+
+// Variables
+let LIST, id;
+
+// get item from localstorage
+let data = localStorage.getItem("TODO");
+
+// check if data is not empty
+if (data) {
+  LIST = JSON.parse(data);
+  id = LIST.length; // set the id to the last one in the list
+  loadList(LIST); // load the list to the user interface
+} else {
+  // if data isn't empty
+  LIST = [];
+  id = 0;
+}
+
+// load items to the user's interface
+function loadList(array) {
+  array.forEach(function (item) {
+    addToDo(item.name, item.id, item.done, item.trash);
+  });
+}
+
+// clear the local storage
+clear.addEventListener("click", function () {
+  localStorage.clear();
+  location.reload();
+});
+
+// Show todays date
+const options = { weekday: "long", month: "short", day: "numeric" };
+const today = new Date();
+
+dateElement.innerHTML = today.toLocaleDateString("en-US", options);
+
+// add to do function
+
+function addToDo(toDo, id, done, trash) {
+  if (trash) {
+    return;
+  }
+
+  const DONE = done ? CHECK : UNCHECK;
+  const LINE = done ? LINE_THROUGH : "";
+
+  const item = `<li class="item">
+                    <p class="text ${LINE}">${toDo}</p>
+                    <div class="${DONE} cmplt" job="complete" id="${id}"></div>
+                    <div class="del" job="delete" id="${id}">🗑</div>
+                  </li>
+                `;
+
+  const position = "beforeend";
+
+  list.insertAdjacentHTML(position, item);
+}
+
+// add an item to the list user the enter key
+function addtask() {}
+
+input.addEventListener("keyup", () => {
+  if (event.keyCode == 13) {
+    const toDo = input.value;
+    if (toDo) {
+      addToDo(toDo, id, false, false);
+
+      LIST.push({
+        name: toDo,
+        id: id,
+        done: false,
+        trash: false,
+      });
+
+      // add item to localstorage ( this code must be added where the LIST array is updated)
+      localStorage.setItem("TODO", JSON.stringify(LIST));
+
+      id++;
+    }
+    input.value = "";
+  }
+});
+
+addbtn.addEventListener("click", () => {
+  const toDo = input.value;
+  if (toDo) {
+    addToDo(toDo, id, false, false);
+
+    LIST.push({
+      name: toDo,
+      id: id,
+      done: false,
+      trash: false,
+    });
+
+    // add item to localstorage ( this code must be added where the LIST array is updated)
+    localStorage.setItem("TODO", JSON.stringify(LIST));
+
+    id++;
+  }
+  input.value = "";
+});
+
+// complete to do
+function completeToDo(element) {
+  element.classList.toggle(CHECK);
+  element.classList.toggle(UNCHECK);
+  element.parentNode.querySelector(".text").classList.toggle(LINE_THROUGH);
+
+  LIST[element.id].done = LIST[element.id].done ? false : true;
+}
+
+// remove to do
+function removeToDo(element) {
+  element.parentNode.parentNode.removeChild(element.parentNode);
+
+  LIST[element.id].trash = true;
+}
+
+// target the items created dynamically
+
+list.addEventListener("click", function (event) {
+  const element = event.target; // return the clicked element inside list
+  // console.log(element);
+  if (
+    element.classList.contains("item") ||
+    element.classList.contains("text")
+  ) {
+    return;
+  }
+  const elementJob = element.attributes.job.value; // complete or delete
+
+  if (elementJob == "complete") {
+    completeToDo(element);
+  } else if (elementJob == "delete") {
+    removeToDo(element);
+  }
+
+  // add item to localstorage ( this code must be added where the LIST array is updated)
+  localStorage.setItem("TODO", JSON.stringify(LIST));
+});
