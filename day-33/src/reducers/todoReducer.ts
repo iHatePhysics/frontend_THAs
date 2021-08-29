fd170ab46a16f424e058659a7e990cb07ac4eeeb
iHatePhysics <Sanskar218.chaware@gmail.com>
2021-08-29 15:48:33 +0530

    fixes

--- day-33/src/reducers/todoReducer.ts
+++ day-33/src/reducers/todoReducer.ts
@@ -1,13 +1,20 @@
-const todoReducer = (state = [], action) => {
+interface ActionType {
+  type: string;
+  payload: string | number;
+}
+
+const todoReducer = (state: string[] = [], action: ActionType) => {
   switch (action.type) {
     case "AddTodo":
       console.log("Added a Todo");
       return [...state, action.payload];
     case "DelTodo":
       console.log("Deleted a Todo");
-      return state.filter((item, index) => index !== action.payload);
+      return state.filter(
+        (item: any, index: number) => index !== action.payload
+      );
     case "SetTodo":
-      console.log(state[action.payload].done);
+      // console.log(state[action.payload].done);
       return (state[action.payload].done = "true");
     case "LoadTodo":
       console.log(action.payload);
